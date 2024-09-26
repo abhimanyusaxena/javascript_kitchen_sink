c274bce2d91756e0c4c3ef6fa86f2235c06d4e02
shrimay18 <shrimaysomani18@gmail.com>
2024-09-26 18:12:49 +0530

    King Move functionality added

--- public/javascript/chess/pieces/king.js
+++ public/javascript/chess/pieces/king.js
@@ -1,11 +1,36 @@
 var King = function(config){
     this.type = 'king';
-    this.constructor(config);
+    Piece.call(this, config); 
 };
 
+King.prototype = Object.create(Piece.prototype); 
+King.prototype.constructor = King; 
 
+King.prototype.isValidPosition = function(targetPosition){
+    // Convert current position to row and column
+    let currentCol = this.position.charAt(0);
+    let currentRow = parseInt(this.position.charAt(1));
 
-King.prototype = new Piece({});
-King.prototype.move = function(newPosition){
+    // Calculate the difference between the target and current positions
+    let colDifference = Math.abs(targetPosition.col.charCodeAt(0) - currentCol.charCodeAt(0));
+    let rowDifference = Math.abs(targetPosition.row - currentRow);
 
+    // The King can move exactly one square in any direction
+    if (colDifference <= 1 && rowDifference <= 1) {
+        return true;
+    }
+
+    // If the move is outside the range of one square, it's invalid
+    console.warn("Invalid move for king");
+    return false;
+}
+
+King.prototype.moveTo = function(targetPosition){
+    // Ensure the move is valid
+    if (this.isValidPosition(targetPosition)) {
+        this.position = targetPosition.col + targetPosition.row;
+        this.render();
+    } else {
+        //NOOP
+    }
 }
