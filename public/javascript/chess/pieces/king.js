269385d1de2f811aae3131ab9d7013c3e57de61e
kdotmanoj <hsuper681@gmail.com>
2024-09-26 17:25:02 +0530

    Add movement logic to King

--- public/javascript/chess/pieces/king.js
+++ public/javascript/chess/pieces/king.js
@@ -3,9 +3,31 @@ var King = function(config){
     this.constructor(config);
 };
 
-
-
 King.prototype = new Piece({});
-King.prototype.move = function(newPosition){
 
+King.prototype.isValidPosition = function(targetPosition){
+    let currentCol = this.position.charAt(0);
+    let currentRow = parseInt(this.position.charAt(1));
+
+    let targetCol = targetPosition.col;
+    let targetRow = parseInt(targetPosition.row);
+
+    let rowDiff = Math.abs(targetRow - currentRow);
+    let colDiff = Math.abs(targetCol.charCodeAt(0) - currentCol.charCodeAt(0));
+
+    if (rowDiff <= 1 && colDiff <= 1) {
+        return true; // Valid move
+    }
+
+    console.warn("Invalid move for king");
+    return false;
+}
+
+King.prototype.moveTo = function(targetPosition){
+    if (this.isValidPosition(targetPosition)) {
+        this.position = targetPosition.col + targetPosition.row;
+        this.render();
+    } else {
+        //NOOP
+    }
 }
