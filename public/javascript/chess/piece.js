46254e287c4090e38445b1eb22614be58ca315cb
AttAditya <attachment.aditya@gmail.com>
2024-09-30 14:51:36 +0530

    Clear Ghost of piece logic and living state of a piece added

--- public/javascript/chess/piece.js
+++ public/javascript/chess/piece.js
@@ -1,6 +1,13 @@
+var livingStates = {
+    ALIVE: "living",
+    DEAD: "dead"
+}
+
 var Piece = function(config){
     this.position = config.position;
     this.color = config.color;
+    this.livingState = livingStates.ALIVE;
+
     if(this.position){
         this.render();        
     }    
@@ -14,32 +21,42 @@ Piece.prototype.attachListeners = function(){
     //To be implemented
 }
 
+Piece.prototype.clearGhost = function() {
+    // Remove the existing piece element from the DOM if it exists
+    if (this.$el && this.$el.parentNode) {
+        this.$el.parentNode.removeChild(this.$el);
+    }
+
+    this.$el = null;
+}
+
 Piece.prototype.render = function(){
+    this.clearGhost();
+
+    if (this.livingState == livingStates.DEAD) {
+        return;
+    }
+
     var col = this.position[0];
     var row = this.position[1];
+
     // Find the li element with matching data-col and data-row attributes
     var element = document.querySelector(`[data-col="${col}"] [data-row="${row}"]`);
-    if (element) {  
-        // Remove the existing piece element from the DOM if it exists
-        if (this.$el && this.$el.parentNode) {
-            this.$el.parentNode.removeChild(this.$el);
-        }
-        // Create a new div element to represent the piece
-        var pieceElement = document.createElement('div');
-        
-        // Add classes to the new element for styling
-        pieceElement.classList.add('piece', this.color, this.type);
-        
-        // Clear any existing content in the cell
-        element.innerHTML = '';
-        
-        // Append the new piece element to the cell
-        element.appendChild(pieceElement);
-        this.$el = pieceElement;
-        this.attachListeners();
-    } else {
+    if (!element) {
         console.warn(`Element not found for position: ${this.position}`);
     }
+
+    // Create a new div element to represent the piece
+    // Add classes to the new element for styling
+    // Clear any existing content in the cell
+    var pieceElement = document.createElement('div');
+    pieceElement.classList.add('piece', this.color, this.type);
+    element.innerHTML = '';
+    
+    // Append the new piece element to the cell
+    element.appendChild(pieceElement);
+    this.$el = pieceElement;
+    this.attachListeners();
 }
 
 Piece.prototype.kill = function(targetPiece){
